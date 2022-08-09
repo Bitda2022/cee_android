2e4b17d6c705fc01a7a33b0077ce15547a0d80ab
sieux2 <zosel154@naver.com>
2022-08-09 17:04:37 +0900

    two

--- app/src/main/java/com/example/cee_project1/activity/MainActivity.kt
+++ app/src/main/java/com/example/cee_project1/activity/MainActivity.kt
@@ -1,21 +1,22 @@
 package com.example.cee_project1.activity
 
-import ViewPagerAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
 import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.example.cee_project1.R
 import com.example.cee_project1.databinding.ActivityMainBinding
-import com.example.cee_project1.databinding.ActivitySplashBinding
 import com.example.cee_project1.fregment.InvestFragment
 import com.example.cee_project1.fregment.QuizSettingFragment
 import com.example.cee_project1.fregment.SettingFragment
 import com.example.cee_project1.fregment.StudyFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,47 +28,63 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initBottomNavigation()
+        binding.pager.isUserInputEnabled = true
+        binding.pager.adapter = ViewPagerAdapter(supportFragmentManager,lifecycle)
+        binding.pager.registerOnPageChangeCallback(PageChangeCallback())
+        binding.bottomNavigationView.setOnItemSelectedListener {navigationSelected(it) }
     }
 
-    private fun initBottomNavigation(){
-
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.pager, StudyFragment())
-            .commitAllowingStateLoss()
-
-        binding.bottomNavigationView.setOnItemSelectedListener{ item ->
-            when (item.itemId) {
-
-                R.id.studyfragment -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.pager, StudyFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
-
-                R.id.quizfragment -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.pager, QuizSettingFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
-                R.id.investfragment -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.pager, InvestFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
-                R.id.settingfragment -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.pager, SettingFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
+    private fun navigationSelected(item: MenuItem): Boolean {
+        val checked = item.setChecked(true)
+        when (checked.itemId) {
+            R.id.studyfragment-> {
+                binding.pager.currentItem = 0
+                return true
             }
-            false
+            R.id.quizfragment -> {
+                binding.pager.currentItem = 1
+                return true
+            }
+            R.id.investfragment -> {
+                binding.pager.currentItem = 2
+                return true
+            }
+            R.id.settingfragment -> {
+                binding.pager.currentItem = 3
+                return true
+            }
+        }
+        return false
+    }
+
+    private inner class ViewPagerAdapter (fragmentManager: FragmentManager,lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager,lifecycle){
+        override fun getItemCount(): Int = 4
+
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 -> StudyFragment()
+                1 -> QuizSettingFragment()
+                2 -> InvestFragment()
+                3 -> SettingFragment()
+                else -> error("no such position: $position")
+            }
+        }
+
+    }
+
+    private inner class PageChangeCallback: ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            super.onPageSelected(position)
+            binding.bottomNavigationView.selectedItemId = when (position) {
+                0 -> R.id.studyfragment
+                1 -> R.id.quizfragment
+                2 -> R.id.investfragment
+                3 -> R.id.settingfragment
+                else -> error("no such position: $position")
+            }
         }
     }
+
 
 }
 
