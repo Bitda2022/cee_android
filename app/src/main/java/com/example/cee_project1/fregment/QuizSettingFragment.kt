7a9756c00daadf1389d5b24e632d46c55de51a94
137pjy <pjy137@konkuk.ac.kr>
2022-08-12 00:51:39 +0900

    feat:quiz fragment and activity layout

--- app/src/main/java/com/example/cee_project1/fregment/QuizSettingFragment.kt
+++ app/src/main/java/com/example/cee_project1/fregment/QuizSettingFragment.kt
@@ -1,10 +1,12 @@
 package com.example.cee_project1.fregment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.cee_project1.activity.QuizActivity
 import com.example.cee_project1.databinding.FragmentInvestBinding
 import com.example.cee_project1.databinding.FragmentQuizSettingBinding
 
@@ -18,8 +20,13 @@ class QuizSettingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentQuizSettingBinding.inflate(inflater, container, false)
+        binding.fragmentQuizSettingStartQuizBtn.setOnClickListener {
+            val QuizIntent: Intent = Intent(activity,QuizActivity::class.java)
+            startActivity(QuizIntent)
+        }
 
         return binding.root
     }
+
 
 }
