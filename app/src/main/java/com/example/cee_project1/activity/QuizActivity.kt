0e60ef4703b37a0782a1d7d9b6c821c0bcd5302e
137pjy <pjy137@konkuk.ac.kr>
2022-08-14 10:55:13 +0900

    feat:create wrong_alert_dialog

--- app/src/main/java/com/example/cee_project1/activity/QuizActivity.kt
+++ app/src/main/java/com/example/cee_project1/activity/QuizActivity.kt
@@ -2,8 +2,10 @@ package com.example.cee_project1.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.example.cee_project1.databinding.ActivityQuizBinding
 import com.example.cee_project1.databinding.ActivitySplashBinding
+import com.example.cee_project1.dialog.WrongAlertDialog
 
 class QuizActivity : AppCompatActivity() {
 
@@ -13,6 +15,16 @@ class QuizActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityQuizBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initView()
+    }
+
+    private fun initView() {
+        //임의로 O를 눌렀을 떄 다이얼로그 뜨도록 구현
+        binding.activityQuizCorrectIv.setOnClickListener {
+            WrongAlertDialog(this){
+
+            }.show()
+        }
     }
 
 }
