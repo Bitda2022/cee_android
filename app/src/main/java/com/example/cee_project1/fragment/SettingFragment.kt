19905eec76b8621236d6de363b3a7be936379b79
sieux2 <zosel154@naver.com>
2022-08-28 21:32:39 +0900

    feat:setting spinner and mail

--- app/src/main/java/com/example/cee_project1/fragment/SettingFragment.kt
+++ app/src/main/java/com/example/cee_project1/fragment/SettingFragment.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
+import com.example.cee_project1.R
 import com.example.cee_project1.databinding.FragmentSettingBinding
+
 
 class SettingFragment : Fragment() {
 
-    lateinit var binding : FragmentSettingBinding
+    lateinit var binding: FragmentSettingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,7 +22,16 @@ class SettingFragment : Fragment() {
     ): View? {
         binding = FragmentSettingBinding.inflate(inflater, container, false)
 
+        eyespinner()
+
         return binding.root
+
     }
 
+    private fun eyespinner() {
+        val handicap = resources.getStringArray(R.array.handicap_level)
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, handicap)
+        binding.setEyeSp.adapter = adapter
+    }
 }
+
