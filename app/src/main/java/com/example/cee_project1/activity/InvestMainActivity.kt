f5719df1a62f6b68a0e79dc8703afd57bd1fdb44
137pjy <pjy137@konkuk.ac.kr>
2022-08-30 19:48:55 +0900

    FEAT:add visibility function of invest select layout

--- app/src/main/java/com/example/cee_project1/activity/InvestMainActivity.kt
+++ app/src/main/java/com/example/cee_project1/activity/InvestMainActivity.kt
@@ -3,10 +3,14 @@ package com.example.cee_project1.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.cee_project1.R
+import com.example.cee_project1.databinding.ActivityInvestMainBinding
+import com.example.cee_project1.databinding.ActivityInvestSelectBinding
 
 class InvestMainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityInvestMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_invest_main)
+        binding = ActivityInvestMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
