f5123bed7a1a80c7520b2fc9f4483a587f803d25
137pjy <pjy137@konkuk.ac.kr>
2022-08-30 18:39:53 +0900

    FEAT:create invest_main and invest_select layout

---
+++ app/src/main/java/com/example/cee_project1/activity/InvestMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cee_project1.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cee_project1.R
+
+class InvestMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_invest_main)
+    }
+}
