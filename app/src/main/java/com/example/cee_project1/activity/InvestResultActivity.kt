d061466ee345c0092d6f3a81011ade9d50aea4bb
jiyeonPark <pjy137@konkuk.ac.kr>
2022-08-31 09:40:17 +0900

    FEAT:create invest result layout

---
+++ app/src/main/java/com/example/cee_project1/activity/InvestResultActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cee_project1.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InvestResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_invest_result)
+    }
+}
