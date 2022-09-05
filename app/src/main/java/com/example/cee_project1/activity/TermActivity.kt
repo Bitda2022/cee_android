e51ff54f5bb54ab642ebc531bc5ea9f02ed043a6
Sang-Min Kang <rdrniel12345@gmail.com>
2022-09-06 00:27:06 +0900

    feat : load last study

--- app/src/main/java/com/example/cee_project1/activity/TermActivity.kt
+++ app/src/main/java/com/example/cee_project1/activity/TermActivity.kt
@@ -1,10 +1,16 @@
 package com.example.cee_project1.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.example.cee_project1.CEEApplication.Companion.prefs
+import com.example.cee_project1.data.Term
 import com.example.cee_project1.databinding.ActivitySplashBinding
 import com.example.cee_project1.databinding.ActivityTermBinding
 import com.example.cee_project1.service.TermVPAdapter
+import io.realm.Realm
+import io.realm.kotlin.where
 
 class TermActivity : AppCompatActivity() {
 
@@ -17,6 +23,21 @@ class TermActivity : AppCompatActivity() {
 
         val termVPAdapter = TermVPAdapter()
         binding.termViewPager.adapter = termVPAdapter
+        binding.termViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                Log.d("viewpager listener", "onPageSelected: $position")
+                val realm = Realm.getDefaultInstance()
+                realm.executeTransaction {
+                    val term : Term? = realm.where<Term>().containsValue("id", position).findFirst()
+                    if(term != null) {
+                        if (!term.hasStudied) {
+                            term.hasStudied = true
+                            prefs.setString("term_save_point", position.toString())
+                        }
+                    }
+                }
+            }
+        })
 
         val id = intent.getIntExtra("id", -1)
         if(id != -1) {
--- app/src/main/java/com/example/cee_project1/fragment/StudyFragment.kt
+++ app/src/main/java/com/example/cee_project1/fragment/StudyFragment.kt
@@ -2,13 +2,18 @@ package com.example.cee_project1.fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.cee_project1.CEEApplication.Companion.prefs
 import com.example.cee_project1.activity.TermActivity
 import com.example.cee_project1.activity.TermListActivity
+import com.example.cee_project1.data.Term
 import com.example.cee_project1.databinding.FragmentStudyBinding
+import io.realm.Realm
+import io.realm.kotlin.where
 
 class StudyFragment : Fragment() {
 
@@ -28,10 +33,19 @@ class StudyFragment : Fragment() {
 
         binding.studyContinueBtn.setOnClickListener {
             val intent = Intent(context, TermActivity::class.java)
+            val id = prefs.getString("term_save_point", "-1")
+            intent.putExtra("id", id.toInt())
             startActivity(intent)
         }
 
         return binding.root
+    }
+
+    override fun onResume() {
+        val pos = prefs.getString("term_save_point", "0").toInt()
+        val text = Realm.getDefaultInstance().where<Term>().containsValue("id", pos).findFirst()?.name
+        binding.studyPositionTv.text = text + "까지"
+        super.onResume()
     }
 
 }
