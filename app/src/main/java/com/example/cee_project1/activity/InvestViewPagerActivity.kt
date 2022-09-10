83b3993f81d578cc348dca7710661ef1cdb919e2
137pjy <pjy137@konkuk.ac.kr>
2022-09-10 16:00:12 +0900

    FEAT:show stories in investFragment from data

--- app/src/main/java/com/example/cee_project1/activity/InvestViewPagerActivity.kt
+++ app/src/main/java/com/example/cee_project1/activity/InvestViewPagerActivity.kt
@@ -1,12 +1,14 @@
 package com.example.cee_project1.activity
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.example.cee_project1.CEEApplication
 import com.example.cee_project1.R
 
 import com.example.cee_project1.databinding.ActivityInvestViewPagerBinding
@@ -21,19 +23,48 @@ class InvestViewPagerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setGame()
         binding = ActivityInvestViewPagerBinding.inflate(layoutInflater)
+
+        var rules=arrayOf("rule1","rule2","rule3")
+        var stories=CEEApplication.gameManager.getNowEventsStory()
+
         fragmentList =ArrayList<Fragment>()
-        fragmentList.add(InvestMainFragment())//게임 규칙 설명
-        fragmentList.add(InvestMainFragment())//스토리
+        fragmentList.add(InvestMainFragment(stories))//게임 규칙 설명
+        fragmentList.add(InvestMainFragment(stories))//스토리
         fragmentList.add(InvestSelectFragment())//투자
         initAdapter()
         setContentView(binding.root)
     }
 
+    private fun setGame() {
+        CEEApplication.gameManager.resetGame()
+        Log.d( "invest_test:현재주차: ",CEEApplication.gameManager.getNowSequence().toString())
+
+
+
+
+
+
+    }
+
     override fun onResume() {
         Log.d("viewpager_lifecycle","investViewPagerActivity onResume 호출")
         if(InvestResultActivity.btnFlag==true){
-            fragmentList.add(2,InvestMainFragment()) //이번 주차 스터리 추가
+
+            if(CEEApplication.gameManager.goNextSequence()){//주차 증가
+                var stories=CEEApplication.gameManager.getNowEventsStory()
+                fragmentList.add(fragmentList.size-1,InvestMainFragment(stories)) //이번 주차 스터리 추가
+            }
+            else{//7주차 -> 다음주차로 넘어가려고 할 때
+                val intent = Intent(this, InvestFinalActivity::class.java)
+                startActivity(intent)
+
+            }
+
+
+            Log.d( "invest_test:현재주차",CEEApplication.gameManager.getNowSequence().toString())
+            var sequence=CEEApplication.gameManager.getNowSequence()
             initAdapter()
             InvestResultActivity.btnFlag=false
             binding.vpSample.currentItem=fragmentList.size-2
